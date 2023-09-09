42920c7c3c0746ad9c8ec61fd16e192aea2ca333
Chris Clark <chris.clark@globalstripe.com>
2023-09-09 23:45:27 +0100

    Update renamer.sh

--- renamer.sh
+++ renamer.sh
@@ -1,3 +1,5 @@
+clear
+#echo "Current IFS: $IFS"
 # OIFS here is a user defined variable that serves to backup the current $IFS Field separator value
 OIFS="$IFS"
 # Reset the field seperator to somethinh else - in this case \n only
@@ -6,18 +8,18 @@ IFS=$'\n'
 
 
 for i in $( ls *.jpg ); do
-     echo "Filename: $i"
+     echo -n "Checking Filename: $i"
      NoSpaces=`echo $i | sed -e 's/ /_/g'`
      TeamFix1=`echo $NoSpaces | sed -e 's/MICH/MIC/g'`
      TeamFix2=`echo $TeamFix1 | sed -e 's/MBFC/MDB/g'`
      TeamFix3=`echo $TeamFix2 | sed -e 's/CFC/CHA/g'`
      NewFileName=$TeamFix3
-     echo "NewFileName: $NewFileName"
      #read x
      if [ "$i" == "$NewFileName" ]; then
-      echo "No Changes to Filename"
+      echo " OK"
      else
-      echo "Renaming File"
+      echo " KO"
+      echo -e "Renaming Filename: $NewFileName"
       mv $i $TeamFix3
      fi
 done
